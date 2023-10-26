0f305605a94dabd14daa3d7762b55b3173227b40
rydhamGEU <149085547+rydhamGEU@users.noreply.github.com>
2023-10-26 20:15:28 +0530

    Add files via upload

---
+++ BigInteger.h
@@ -0,0 +1,36 @@
+/*
+
+Name - Rydham Gupta
+section - ( CSE -B )
+class roll no. - 49
+University Roll No. - 2021398
+Assignment submitted to - Mr. Akshay Rajput Sir
+
+*/
+
+
+#ifndef BIG_INTEGER_H
+#define BIG_INTEGER_H
+struct node 
+{
+    int data;
+    struct node* next;
+};
+struct BigInteger 
+{
+    struct node* l;
+    int length;
+    int sign;
+};
+void push(struct node **h, int val);
+struct BigInteger initialize(char *s);
+void truncate(struct BigInteger* num);
+struct BigInteger add(struct BigInteger a, struct BigInteger b);
+int length(struct node *ptr);
+struct BigInteger sub(struct BigInteger a, struct BigInteger b);
+struct BigInteger mul(struct BigInteger a, struct BigInteger b);
+struct BigInteger div1(struct BigInteger a, struct BigInteger b);
+struct BigInteger mod(struct BigInteger a, struct BigInteger b);
+void freeLinkedList(struct node *head);
+void print(struct node *res);
+#endif
